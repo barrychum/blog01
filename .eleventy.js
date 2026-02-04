c339f22c8a88537a63dcd9e7099387e896a125b6
Barry <barrychum@gmail.com>
2026-02-04 08:09:06 +0000

    test

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,16 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
+    // Read Time Filter
+  eleventyConfig.addFilter("readTime", (content) => {
+    const wordsPerMinute = 200;
+    // Strip HTML tags to get a clean word count
+    const text = content.replace(/<[^>]*>/g, "");
+    const wordCount = text.split(/\s+/).length;
+    const readingTime = Math.ceil(wordCount / wordsPerMinute);
+    return `${readingTime} min read`;
+  });
+
   return {
     dir: {
       input: ".",
@@ -13,3 +23,4 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
+
